0c16f32d3a47df18915b54bc6d93c77b56ddf1af
gautamsajjan <sajjangautamc.18@gmail.com>
2024-12-06 23:57:18 +0545

    Sign up Page updated

--- Frontend/src/Components/Header1/Header1.jsx
+++ Frontend/src/Components/Header1/Header1.jsx
@@ -6,10 +6,13 @@ const Header1 = () => {
     <div className='Header1'>
        <div className="Header-text">
             <p>Welcome to the Farmer Dashboard</p>
-            <button class="update">Add product !</button>
+            <button className="update">Add product !</button>
         </div>
     </div>
   )
 }
 
 export default Header1
+
+//i have change in line no 9 pervious it was <button class="update">Add product !</button>  
+// and now i have changed an changed code is <button className="update">Add product !</button>
